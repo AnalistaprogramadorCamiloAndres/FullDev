08f0d8def99c2e383f85c3e8709fc3bf4c869a80
CamiloRodriguez <camiloa_rodrigueze@soy.sena.edu.co>
2024-08-04 17:18:13 -0500

    login

--- login.js
+++ login.js
@@ -1,16 +1,22 @@
-const user = "adm";
-const password = "123";
+const user = [{ id: 1, usuario: 'usuario1', clave: '1234' },
+              { id: 2, usuario: 'usuario2', clave: '5678' },
+              { id: 3, usuario: 'usuario3', clave: '9101' },
+              { id: 4, usuario: 'usuario4', clave: '1121' },
+              { id: 5, usuario: 'usuario5', clave: '3141' }]
 
 
-function submitbottom (){
+
+function submitbottom() {
 
     let usuario = document.getElementById("us").value;
     let contrasena = document.getElementById('pw').value;
-
-    if (user == usuario && password == contrasena) {
-        window.open ('principal.html');
-        console.log ('ingreso correcto'); 
+    const resultado = user.find(user => user.usuario === usuario && user.clave === contrasena);
+    if (resultado) {
+        window.open('principal.html');
+        console.log('ingreso correcto');
     } else {
-        console.log ('error en las credenciales');
+        console.log('error en las credenciales');
+        alert('erro en las credenciales');
     }
 }
+
